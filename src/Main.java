051d1f9ec0129264344b190c6fb80cf1459f255f
dfern0413 <dfern0413@gmail.com>
2022-11-18 10:22:26 -0500

    BubbleSort algorithm added to repository.

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        //Make a variable to hold value when swaping
+        //Make a variable to hold value when swapping
         int temp;
         // Array to sort
-        int[] Array = {115, 17, 5, 3, 20, 0, 19, 2 , 8};
+        int[] array = {115, 17, 5, 3, 20, 0, 19, 2 , 8, 888};
         // 2 for loops, one iterates through the array doing the swapping and the other makes sure the second array runs
         // enough time to sort array
-        for (int i = 0; i < Array.length - 1; i++) {
-            for (int j = 0; j < Array.length-1; j++) {
-                if (Array[j] > Array[j+1]) {
-                    temp = Array[j];
-                    Array[j] = Array[j+1];
-                    Array[j+1] = temp;
+        for (int i = 0; i < array.length-1; i++) {
+            for (int j = 0; j < array.length-1; j++) {
+                if (array[j] > array[j+1]) {
+                    temp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = temp;
                 }
             }
         }
         // Print the bubble sorted array
-        for (int i : Array) {
+        for (int i : array) {
             System.out.println(i);
         }
     }
